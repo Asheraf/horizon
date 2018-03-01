843567195b8af4a4ba17ff917a7785f7b18dcd31
Smokexyz <sagunkho@hotmail.com>
2018-03-01 14:55:58 +0800

    Added Locked Lookup Table (Thread-safe) Added LockedLookupTable Tests

---
+++ src/Core/Multithreading/LockedLookupTable.hpp
@@ -0,0 +1,148 @@
+/***************************************************
+ *       _   _            _                        *
+ *      | | | |          (_)                       *
+ *      | |_| | ___  _ __ _ _______  _ __          *
+ *      |  _  |/ _ \| '__| |_  / _ \| '_  \        *
+ *      | | | | (_) | |  | |/ / (_) | | | |        *
+ *      \_| |_/\___/|_|  |_/___\___/|_| |_|        *
+ ***************************************************
+ * This file is part of Horizon (c).
+ * Copyright (c) 2018 Horizon Dev Team.
+ *
+ * Base Author - Sagun Khosla. (sagunxp@gmail.com)
+ *
+ * Under a proprietary license this file is not for use
+ * or viewing without permission.
+ **************************************************/
+
+#ifndef HORIZON_CORE_MULTITHREADING_LOCKEDLOOKUPTABLE_HPP
+#define HORIZON_CORE_MULTITHREADING_LOCKEDLOOKUPTABLE_HPP
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <boost/thread/shared_mutex.hpp>
+#include <memory>
+#include <list>
+#include <vector>
+
+enum lookup_table_insert_type
+{
+	LTI_ADDED,
+	LTI_UPDATED,
+};
+
+template <typename Key, typename Value, typename Hash=std::hash<Key>>
+class LockedLookupTable
+{
+class bucket_type;
+public:
+	typedef Key key_type;
+	typedef Value mapped_type;
+	typedef Hash hash_type;
+
+	LockedLookupTable(unsigned num_buckets = 19, const Hash &hasher_ = Hash())
+	: buckets(num_buckets), hasher(hasher_)
+	{
+		for (unsigned i = 0; i < num_buckets; ++i)
+			buckets[i].reset(new bucket_type);
+	}
+
+	LockedLookupTable(const LockedLookupTable &other) = delete;
+	LockedLookupTable &operator=(const LockedLookupTable &other) = delete;
+
+	Value at(Key const &key, Value const &default_value = Value()) const
+	{
+		return get_bucket(key).at(key, default_value);
+	}
+
+	lookup_table_insert_type insert(const Key &key, const Value &value)
+	{
+		return get_bucket(key).insert(key, value);
+	}
+
+	void erase(const Key &key)
+	{
+		get_bucket(key).erase(key);
+	}
+
+	std::map<Key, Value> get_map() const
+	{
+		std::vector<boost::unique_lock<boost::shared_mutex>> locks;
+		std::map<Key, Value> res;
+
+		for (unsigned i = 0;  i < buckets.size(); ++i)
+			locks.push_back(boost::unique_lock<boost::shared_mutex>(buckets[i]->_mutex));
+
+		for (unsigned i = 0; i < buckets.size(); ++i) {
+			for (typename bucket_type::bucket_iterator it = buckets[i]->data.begin();
+				 it != buckets[i]->data.end();
+				 ++it) {
+				res.insert(*it);
+			}
+		}
+
+		return res;
+	}
+
+	int max_collisions() { return buckets[0]->data.size() ? buckets[0]->data.size() - 1 : 0; }
+
+private:
+	std::vector<std::unique_ptr<bucket_type>> buckets;
+	Hash hasher;
+
+	bucket_type &get_bucket(Key const &key) const
+	{
+		const std::size_t bucket_index = hasher(key) % buckets.size();
+		return *buckets[bucket_index];
+	}
+
+	class bucket_type
+	{
+		typedef std::pair<Key, Value> bucket_value;
+		typedef std::list<bucket_value> bucket_data;
+	public:
+		typedef typename bucket_data::iterator bucket_iterator;
+		Value at(Key const &key, Value const &default_value)
+		{
+			boost::shared_lock<boost::shared_mutex> lock(_mutex);
+			bucket_iterator const found_entry = find_entry_for(key);
+			return (found_entry == data.end()) ? default_value : found_entry->second;
+		}
+
+		lookup_table_insert_type insert(Key const &key, Value const &value)
+		{
+			std::unique_lock<boost::shared_mutex> lock(_mutex);
+			bucket_iterator found_entry = find_entry_for(key);
+			if (found_entry == data.end()) {
+				data.push_back(bucket_value(key, value));
+				return LTI_ADDED;
+			} else {
+				found_entry->second = value;
+				return LTI_UPDATED;
+			}
+		}
+
+		void erase(Key const &key)
+		{
+			std::unique_lock<boost::shared_mutex> lock(_mutex);
+			bucket_iterator const found_entry = find_entry_for(key);
+			if (found_entry != data.end())
+				data.erase(found_entry);
+		}
+
+		bucket_data data;
+		mutable boost::shared_mutex _mutex;
+	private:
+		bucket_iterator find_entry_for(Key const &key)
+		{
+			return std::find_if(data.begin(), data.end(),
+				[&](bucket_value const &item)
+				{
+					return item.first == key;
+				});
+		}
+	};
+};
+
+#endif /* HORIZON_CORE_MULTITHREADING_LOCKEDLOOKUPTABLE_HPP */
---
+++ src/Tests/LockedLookupTableTest.cpp
@@ -0,0 +1,212 @@
+//
+// Created by SagunKho on 28/01/2018.
+//
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "LockedLookupTableTest"
+
+#include "Core/Multithreading/LockedLookupTable.hpp"
+#include <boost/test/unit_test.hpp>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+
+#define MAX_THREADS 2
+#define MAX_TABLE_SIZE 500000
+#define STRING_SIZE 32
+#define MAX_BUCKETS MAX_TABLE_SIZE / 10
+
+#define SPLIT_THREADS 1
+
+BOOST_AUTO_TEST_CASE(LockedLookupTableTest)
+{
+	clock_t int_begin = clock();
+	LockedLookupTable<int, int> table(MAX_BUCKETS);
+	std::atomic<bool> go{false};
+	std::thread *r[MAX_THREADS], *w[MAX_THREADS];
+
+#if SPLIT_THREADS
+	printf("Dividing work between %d reader + %d writer threads: %d per thread.\n", MAX_THREADS, MAX_THREADS, (MAX_TABLE_SIZE/MAX_THREADS));
+#endif
+
+	// Populate
+	for (int j = 0; j < MAX_TABLE_SIZE; j++)
+		table.insert(j, j);
+
+	// Readers
+	for (int i = 0; i < MAX_THREADS; i++) {
+		r[i] = new std::thread([&go, &table, i] () {
+#if SPLIT_THREADS
+			int start = MAX_TABLE_SIZE / MAX_THREADS * i;
+			int end = start + MAX_TABLE_SIZE / MAX_THREADS;
+#else
+			int start = 0;
+			int end = MAX_TABLE_SIZE;
+#endif
+			while (!go);
+			clock_t t_begin = clock();
+
+			for (int j = start; j < end; j++) {
+				int val = table.at(j);
+				BOOST_CHECK_EQUAL(j, val);
+				if (j != val) {
+					char output[256];
+					sprintf(output, "INT/INT not equal: %d != %d\n", j, val);
+					BOOST_FAIL(output);
+				}
+			}
+			printf("INT/INT Thread #%d - Read Time %.5fs\n", i, double(clock() - t_begin) / CLOCKS_PER_SEC);
+		});
+	}
+
+	// Writers
+	for (int i = 0; i < MAX_THREADS; i++) {
+		w[i] = new std::thread([&go, &table, i] () {
+#if SPLIT_THREADS
+			int start = MAX_TABLE_SIZE / MAX_THREADS * i;
+			int end = start + MAX_TABLE_SIZE / MAX_THREADS;
+#else
+			int start = 0;
+			int end = MAX_TABLE_SIZE;
+#endif
+			while (!go);
+			clock_t t_begin = clock();
+
+			for (int j = start; j < end; j++)
+				table.insert(j, j);
+
+			printf("INT/INT Thread #%d - Write Time %.5fs\n", i, double(clock() - t_begin) / CLOCKS_PER_SEC);
+		});
+	}
+
+	go.exchange(true);
+
+	for (int i = 0; i < MAX_THREADS; i++) {
+		r[i]->join();
+		w[i]->join();
+		delete r[i];
+		delete w[i];
+	}
+
+	std::cout << "INT/INT Read/Write Test Finished in " << double(clock() - int_begin) / CLOCKS_PER_SEC << "s" << std::endl;
+
+	int_begin = clock();
+
+	// Map Iteration
+	int i = 0;
+	for (auto c : table.get_map()) {
+		BOOST_CHECK_EQUAL(i, c.second);
+		i++;
+	}
+	std::cout << "INT/INT Map Iteration Test Finished in " << double(clock() - int_begin) / CLOCKS_PER_SEC << "s" << std::endl;
+
+	// Final Integrity Check
+	for (int j = 0; j < MAX_TABLE_SIZE; j++)
+		BOOST_CHECK_EQUAL(j, table.at(j));
+
+	std::cout << "INT/INT Finished in " << double(clock() - int_begin) / CLOCKS_PER_SEC << "s" << std::endl;
+	std::cout << "INT/INT Collisions: " << table.max_collisions() << std::endl;
+
+	/*==================================*
+	 * String Key / Value Test
+	 *==================================*/
+	clock_t str_begin = clock();
+	std::string chars("abcdefghijklmnopqrstuvwxyz"
+					  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+					  "1234567890"
+					  "!@#$%^&*()"
+					  "`~-_=+[{]}\\|;:'\",<.>/? ");
+	LockedLookupTable<std::string, std::string> str_table(MAX_BUCKETS);
+	std::vector<std::string> keylist, vallist;
+
+	go.exchange(false);
+
+	// Populate
+	for (int j = 0; j < MAX_TABLE_SIZE; j++) {
+		char key[STRING_SIZE + 1]{0}, val[STRING_SIZE + 1]{0};
+		for (int i = 0; i < STRING_SIZE; ++i) {
+			key[i] = chars[rand() % (chars.size() - 1)];
+			val[i] = chars[rand() % (chars.size() - 1)];
+		}
+
+		keylist.push_back(key);
+		vallist.push_back(val);
+		str_table.insert(key, val);
+	}
+
+	// Readers
+	for (int i = 0; i < MAX_THREADS; i++) {
+		r[i] = new std::thread([&go, &str_table, &keylist, &vallist, i] () {
+#if SPLIT_THREADS
+			int start = MAX_TABLE_SIZE / MAX_THREADS * i;
+			int end = start + MAX_TABLE_SIZE / MAX_THREADS;
+#else
+			int start = 0;
+			int end = MAX_TABLE_SIZE;
+#endif
+			while (!go);
+
+			clock_t t_begin = clock();
+
+			for (int j = start; j < end; j++) {
+				std::string val = str_table.at(keylist.at(j));
+				if (val.compare(vallist.at(j)) != 0) {
+					printf("STR/STR not equal: %s != %s\n", vallist.at(j).c_str(), val.c_str());
+				}
+			}
+			printf("STR/STR Thread #%d - Read Time %.5fs\n", i, double(clock() - t_begin) / CLOCKS_PER_SEC);
+		});
+	}
+
+	// Writers
+	for (int i = 0; i < MAX_THREADS; i++) {
+		w[i] = new std::thread([&go, &str_table, &keylist, &vallist, i] () {
+#if SPLIT_THREADS
+			int start = MAX_TABLE_SIZE / MAX_THREADS * i;
+			int end = start + MAX_TABLE_SIZE / MAX_THREADS;
+#else
+			int start = 0;
+			int end = MAX_TABLE_SIZE;
+#endif
+			while (!go);
+			clock_t t_begin = clock();
+
+			for (int j = start; j < end;  j++)
+				str_table.insert(keylist.at(j), vallist.at(j));
+
+			printf("STR/STR Thread #%d - Write Time %.5fs\n", i, double(clock() - t_begin) / CLOCKS_PER_SEC);
+		});
+	}
+
+	go.exchange(true);
+
+	for (int i = 0; i < MAX_THREADS; i++) {
+		r[i]->join();
+		w[i]->join();
+		delete r[i];
+		delete w[i];
+	}
+
+	std::cout << "STR/STR Read/Write Test Finished in " << double(clock() - str_begin) / CLOCKS_PER_SEC << "s" << std::endl;
+
+	str_begin = clock();
+
+	// Map Iteration
+	std::map<std::string, std::string> strmap = str_table.get_map();
+	i = 0;
+	for (auto str : keylist) {
+		BOOST_CHECK_EQUAL(strmap.at(str).compare(vallist.at(i)), 0);
+		i++;
+	}
+
+	std::cout << "STR/STR Map Iteration Test Finished in " << double(clock() - str_begin) / CLOCKS_PER_SEC << "s" << std::endl;;
+
+	// Final Integrity Check
+	i = 0;
+	for (auto str : keylist) {
+		BOOST_CHECK_EQUAL(vallist.at(i).compare(str_table.at(str)), 0);
+		i++;
+	}
+
+	std::cout << "STR/STR Collisions: " << str_table.max_collisions() << std::endl;
+}
