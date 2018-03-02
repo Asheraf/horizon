ad90d9759ee843b62d58e06f2f6e9f877333c191
Smokexyz <sagunkho@hotmail.com>
2018-03-02 11:08:56 +0800

    Added ThreadSafeQueue unit test

---
+++ src/Core/Multithreading/ThreadSafeQueue.hpp
@@ -0,0 +1,103 @@
+/***************************************************
+ *       _   _            _                        *
+ *      | | | |          (_)                       *
+ *      | |_| | ___  _ __ _ _______  _ __          *
+ *      |  _  |/ _ \| '__| |_  / _ \| '_  \        *
+ *      | | | | (_) | |  | |/ / (_) | | | |        *
+ *      \_| |_/\___/|_|  |_/___\___/|_| |_|        *
+ ***************************************************
+ * This file is part of Horizon (c).
+ * Copyright (c) 2018 Horizon Dev Team.
+ *
+ * Under a proprietary license this file is not for use
+ * or viewing without permission.
+ **************************************************/
+
+#ifndef HORIZON_CORE_MULTITHREADING_THREADSAFEQUEUE_HPP
+#define HORIZON_CORE_MULTITHREADING_THREADSAFEQUEUE_HPP
+
+#include <memory>
+#include <mutex>
+
+template<typename T>
+class ThreadSafeQueue
+{
+private:
+    struct node
+    {
+        std::shared_ptr<T> data;
+        std::unique_ptr<node> next;
+    };
+
+    std::mutex head_mutex;
+    std::unique_ptr<node> head;
+    std::mutex tail_mutex;
+    node *tail;
+
+    node *get_tail()
+    {
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        return tail;
+    }
+
+    std::unique_ptr<node> pop_head()
+    {
+        std::lock_guard<std::mutex> head_lock(head_mutex);
+        if(head.get() == get_tail())
+            return nullptr;
+
+        std::unique_ptr<node> old_head = std::move(head);
+        head = std::move(old_head->next);
+        return old_head;
+    }
+
+public:
+    ThreadSafeQueue():
+        head(new node), tail(head.get())
+    {}
+
+	~ThreadSafeQueue()
+	{
+		std::size_t qsize = size();
+
+		for (std::size_t i = 0; i < qsize; i++)
+			try_pop();
+	}
+
+    ThreadSafeQueue(const ThreadSafeQueue &other)=delete;
+    ThreadSafeQueue &operator=(const ThreadSafeQueue &other)=delete;
+
+    std::shared_ptr<T> try_pop()
+    {
+        std::unique_ptr<node> old_head = pop_head();
+        return old_head ? old_head->data : std::shared_ptr<T>();
+    }
+
+    void push(T &&new_value)
+    {
+        std::shared_ptr<T> new_data(
+            std::make_shared<T>(std::move(new_value)));
+        std::unique_ptr<node> p(new node);
+		node *new_tail = p.get();
+        std::lock_guard<std::mutex> tail_lock(tail_mutex);
+        tail->data = new_data;
+        tail->next = std::move(p);
+        tail = new_tail;
+    }
+
+	std::size_t size()
+	{
+		int count = 0;
+		std::lock_guard<std::mutex> head_lock(head_mutex);
+		std::lock_guard<std::mutex> tail_lock(tail_mutex);
+		node const *n = head.get();
+
+		while ((n = n->next.get())) {
+			count++;
+		}
+
+		return count;
+	}
+};
+
+#endif // HORIZON_CORE_MULTITHREADING_THREADSAFEQUEUE_HPP
---
+++ src/Tests/ThreadSafeQueueTest.cpp
@@ -0,0 +1,71 @@
+/***************************************************
+ *       _   _            _                        *
+ *      | | | |          (_)                       *
+ *      | |_| | ___  _ __ _ _______  _ __          *
+ *      |  _  |/ _ \| '__| |_  / _ \| '_  \        *
+ *      | | | | (_) | |  | |/ / (_) | | | |        *
+ *      \_| |_/\___/|_|  |_/___\___/|_| |_|        *
+ ***************************************************
+ * This file is part of Horizon (c).
+ * Copyright (c) 2018 Horizon Dev Team.
+ *
+ * Base Author - Sagun Khosla <sagunxp@gmail.com>
+ *
+ * Under a proprietary license this file is not for use
+ * or viewing without permission.
+ **************************************************/
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "ThreadSafeQueueTest"
+
+#include "Core/Multithreading/ThreadSafeQueue.hpp"
+#include <boost/test/unit_test.hpp>
+#include <cstring>
+#include <thread>
+
+#define MAX_PUSHES 1000000
+
+BOOST_AUTO_TEST_CASE(ThreadSafeQueueTest)
+{
+	ThreadSafeQueue<int> queue;
+	std::thread *t[4];
+	std::atomic<bool> go;
+
+	for (int i = 0; i < MAX_PUSHES; i++)
+		queue.push(i);
+
+	BOOST_CHECK_EQUAL(queue.size(), MAX_PUSHES);
+
+	t[0] = new std::thread([&queue, &go]() {
+		while (!go);
+		for (int i = 0; i < MAX_PUSHES; i++)
+			queue.push(i);
+	});
+
+	t[1] = new std::thread([&queue, &go]() {
+		while (!go);
+		for (int i = 0; i < MAX_PUSHES; i++)
+			queue.push(i);
+	});
+
+	t[2] = new std::thread([&queue, &go]() {
+		while (!go);
+		for (int i = 0; i < MAX_PUSHES; i++)
+			queue.try_pop();
+	});
+
+	t[3] = new std::thread([&queue, &go]() {
+		while (!go);
+		for (int i = 0; i < MAX_PUSHES; i++)
+			queue.try_pop();
+	});
+
+	go.exchange(true);
+
+	t[0]->join();
+	t[1]->join();
+	t[2]->join();
+	t[3]->join();
+
+	BOOST_CHECK_EQUAL(queue.size(), MAX_PUSHES);
+}
